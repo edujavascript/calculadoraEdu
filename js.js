ca38285b2e58c7584a9b43b0025d9fc1e17c1043
Daniel <43273980+dbistuer@users.noreply.github.com>
2018-10-31 20:44:41 +0100

    Separo el javascript de l'html

---
+++ js.js
@@ -0,0 +1,44 @@
+ function escriure(n){
+	var caixa2 = document.forma.caixa2.value;
+	if (borrar) {
+		document.forma.caixa2.value="";
+		borrar = false;
+		document.forma.caixa2.value = n;
+	}else if (caixa2 == "0" && n != "."){
+		cajao = caixa2.replace("0", "")
+		document.forma.caixa2.value = cajao + n;
+	}
+	else{
+		document.forma.caixa2.value = caixa2 + n;
+	}
+}
+
+var borrar = false;
+
+function operacio(o){
+	var caixa1 = document.forma.caixa1.value;
+	var caixa2 = document.forma.caixa2.value;
+	var unNum = caixa1.substring(caixa1.length-1);
+	calcular()
+	if (unNum == "+" || unNum == "-" || unNum=="*" || unNum=="/") {
+		unNum = unNum.replace(unNum,o);
+		var res = caixa1.substring(0,caixa1.length-1);
+		document.forma.caixa1.value = res+unNum;
+	}
+	if (caixa1 == "" && caixa2 != ""){
+		document.forma.caixa1.value = caixa2 + o;
+	}
+	else{
+		document.forma.caixa1.value = caixa1 + caixa2 + o;
+	}
+	borrar = true;
+}
+
+function calcular(){
+	var caixa1 = document.forma.caixa1.value;
+	var caixa2 = document.forma.caixa2.value;
+	document.forma.caixa2.value = eval(caixa1 + caixa2);
+	document.forma.caixa1.value = "";
+	borrar = true;
+	blocdel = true;
+}
